9a5c32931f6d87bd992725c3a6c4cb1424643d15
martin <nftmartin@pm.me>
2022-10-25 01:53:52 +0200

    changes

--- WolfgameggMainScript.js
+++ WolfgameggMainScript.js
@@ -183,7 +183,7 @@ function search(searchQuery){
                             getLandData(landID.community);
                             
                         }
-                        $(document).load(function() {
+                        $( "#mapWrapper" ).load(function() {
                             console.log('dom loaded');
                             community.value = landID.community;
                             $('.div-overlay-color').css( 'display', 'block');
