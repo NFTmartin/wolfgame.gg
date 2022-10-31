e8aa2a5d44c5a3769aed39a9395526894b175271
martin <nftmartin@pm.me>
2022-10-31 23:49:37 +0100

    fix

--- WolfgameggMainScript.js
+++ WolfgameggMainScript.js
@@ -133,7 +133,7 @@ function checkCookies(){
     console.log('check cookies started')
     // - hide welcome
     console.log(getCookie('hideWelcome'))
-    if (getCookie('hideWelcome') === false){
+    if (getCookie('hideWelcome') === "false"){
         welcome_screen.style.display = 'flex';
         console.log('welcome set to flex')
     } else if ( getCookie('hideWelcome') === "") {
